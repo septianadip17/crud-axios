e0f364875490638950289810cadfc11bd83c56f6
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-27 15:40:13 +0700

    data showed

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default Home
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/users")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center bg-light vh-100">
+      <h1>List of Users</h1>
+      <div className="w-75 rounded bg-white border shadow p-4">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((d, i) => (
+              <tr key={i}>
+                <td>{d.id}</td>
+                <td>{d.name}</td>
+                <td>{d.email}</td>
+                <td>{d.phone}</td>
+                <td>
+                  <button className="btn btn-sm btn-primary">edit</button>
+                  <button className="btn btn-sm btn-danger">delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
