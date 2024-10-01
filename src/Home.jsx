f5468f032b32fcf47dd988db908d0113d7547870
Septian Adi Pratama <septianadip7998@gmail.com>
2024-10-01 13:40:27 +0700

    delete success

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
   const [data, setData] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -12,12 +13,27 @@ const Home = () => {
       .catch((err) => console.log(err));
   }, []);
 
+  const handleDelete = (id) => {
+    const confirm = window.confirm("Would you like to delete?");
+    if (confirm) {
+      axios
+        .delete("http://localhost:3000/users/" + id)
+        .then((res) => {
+          location.reload()
+          navigate("/");
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
   return (
     <div className="d-flex flex-column justify-content-center align-items-center bg-light vh-100">
       <h1>List of Users</h1>
       <div className="w-75 rounded bg-white border shadow p-4">
         <div className="d-flex justify-content-end">
-          <Link to="/create" className="btn btn-success">Add +</Link>
+          <Link to="/create" className="btn btn-success">
+            Add +
+          </Link>
         </div>
         <table className="table table-striped">
           <thead>
@@ -37,9 +53,24 @@ const Home = () => {
                 <td>{d.email}</td>
                 <td>{d.phone}</td>
                 <td>
-                  <Link to={`/read/${d.id}`} className="btn btn-sm btn-info me-2">Read</Link>
-                  <button className="btn btn-sm btn-primary me-2">Edit</button>
-                  <button className="btn btn-sm btn-danger">Delete</button>
+                  <Link
+                    to={`/read/${d.id}`}
+                    className="btn btn-sm btn-info me-2"
+                  >
+                    Read
+                  </Link>
+                  <Link
+                    to={`/update/${d.id}`}
+                    className="btn btn-sm btn-primary me-2"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    onClick={(e) => handleDelete(d.id)}
+                    className="btn btn-sm btn-danger"
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
