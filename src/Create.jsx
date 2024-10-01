c7538eb8f11353b7e2e2e052050a07f7a862d291
Septian Adi Pratama <septianadip7998@gmail.com>
2024-10-01 11:44:07 +0700

    form showed

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Create = () => {
   return (
-    <div>
-      <h1>Create</h1>
+    <div className="d-flex w-100 vh-100 justify-content-center align-items-center bg-light">
+      <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
+        <h1>Add a User</h1>
+        <form action="">
+          <div className="mb-2">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              placeholder="Enter Name"
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Enter Email"
+            />
+          </div>
+          <div className="mb2">
+            <label htmlFor="phone">Phone:</label>
+            <input
+              type="text"
+              name="phone"
+              className="form-control"
+              placeholder="Enter Phone"
+            />
+            <button className="btn btn-success">Submit</button>
+            <Link to="/">Back</Link>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
