301e8a548afe3a388517ab9573629a731d1f95de
Septian Adi Pratama <septianadip7998@gmail.com>
2024-10-01 11:34:41 +0700

    add button direct to create

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const [data, setData] = useState([]);
@@ -11,11 +12,13 @@ const Home = () => {
       .catch((err) => console.log(err));
   }, []);
 
-  
   return (
     <div className="d-flex flex-column justify-content-center align-items-center bg-light vh-100">
       <h1>List of Users</h1>
       <div className="w-75 rounded bg-white border shadow p-4">
+        <div className="d-flex justify-content-end">
+          <Link to="/create" className="btn btn-success">Add +</Link>
+        </div>
         <table className="table table-striped">
           <thead>
             <tr>
