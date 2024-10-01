d4ed98534bd031b079176e48e6f734c9e8b9da36
Septian Adi Pratama <septianadip7998@gmail.com>
2024-10-01 11:54:55 +0700

    css fixed

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,12 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Create = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3000/users", values)
+      .then((res) => {
+        console.log(res);
+        navigate('/')
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="d-flex w-100 vh-100 justify-content-center align-items-center bg-light">
       <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
         <h1>Add a User</h1>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div className="mb-2">
             <label htmlFor="name">Name:</label>
             <input
@@ -14,6 +33,7 @@ const Create = () => {
               name="name"
               className="form-control"
               placeholder="Enter Name"
+              onChange={(e) => setValues({ ...values, name: e.target.value })}
             />
           </div>
           <div className="mb-2">
@@ -23,6 +43,7 @@ const Create = () => {
               name="email"
               className="form-control"
               placeholder="Enter Email"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
             />
           </div>
           <div className="mb2">
@@ -32,6 +53,7 @@ const Create = () => {
               name="phone"
               className="form-control"
               placeholder="Enter Phone"
+              onChange={(e) => setValues({ ...values, phone: e.target.value })}
             />
             <button className="btn btn-success">Submit</button>
             <Link to="/">Back</Link>
