5817926c081f7603461661c1cb99d9d305715919
Septian Adi Pratama <septianadip7998@gmail.com>
2024-10-01 13:33:31 +0700

    update succesfull

--- src/Update.jsx
+++ src/Update.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+Link;
 
 const Update = () => {
-  return (
-    <div>Update</div>
-  )
-}
+  // const [data, setData] = useState([]);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-export default Update
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/users/" + id)
+      .then((res) => {
+        setValues(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    axios
+      .put("http://localhost:3000/users/" + id, values)
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="d-flex w-100 vh-100 justify-content-center align-items-center bg-light">
+      <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
+        <h1>Update User</h1>
+        <form onSubmit={handleUpdate}>
+          <div className="mb-2">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              placeholder="Enter Name"
+              value={values.name}
+              onChange={(e) => setValues({ ...values, name: e.target.value })}
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Enter Email"
+              value={values.email}
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
+            />
+          </div>
+          <div className="mb2">
+            <label htmlFor="phone">Phone:</label>
+            <input
+              type="text"
+              name="phone"
+              className="form-control"
+              placeholder="Enter Phone"
+              value={values.phone}
+              onChange={(e) => setValues({ ...values, phone: e.target.value })}
+            />
+            <div className="d-flex gap-2 mt-3">
+              <button className="btn btn-success">Update</button>
+              <Link to="/" className="btn btn-primary">
+                Back
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Update;
